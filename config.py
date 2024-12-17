82e1d26693cf580f2d7ce3cb97df3d1111f6fe09
Abhilash-H-T <abhilashht08@gmail.com>
2024-12-17 17:48:37 +0530

    Deleted old files to avoid confusion

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///files.db'  # Using SQLite for simplicity
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max upload size
-    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
-    ALLOWED_EXTENSIONS = {'txt'}
